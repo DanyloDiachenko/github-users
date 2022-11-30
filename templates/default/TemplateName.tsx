dcbc13699b16fcacd705dbf98eb24015002ec669
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-30 13:21:50 +0200

    _

--- templates/default/TemplateName.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './TemplateName.module.scss';
-
-interface TemplateNameProps { }
-
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
-);
