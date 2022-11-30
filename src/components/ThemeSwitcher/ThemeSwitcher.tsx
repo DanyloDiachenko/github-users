3199ba296d5a9bdb8214bf63cc3bd37e899a923e
Danylo Diachenko <danildiachenko23@gmail.com>
2022-11-30 18:07:29 +0200

    Update ThemeSwitcher.tsx

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import styles from './ThemeSwitcher.module.scss';
+import { ReactComponent as MoonIcon } from 'assets/icon-moon.svg'
+import { ReactComponent as SunIcon } from 'assets/icon-sun.svg'
 
 interface ThemeSwitcherProps { }
 
-export const ThemeSwitcher = ({ }: ThemeSwitcherProps) => (
-  <div className={styles.themeSwitcher}>
-    ThemeSwitcher Component
-  </div>
-);
+export const ThemeSwitcher = ({ }: ThemeSwitcherProps) => {
+
+  const [isDark, setDark] = useState(false);
+  const themeText = isDark ? 'Light' : 'Dark';
+  const ThemeIcon = isDark ? SunIcon : MoonIcon;
+
+  return (
+    <div className={styles.themeSwitcher}>
+      ThemeSwitcher Component
+    </div>
+  );
+};
