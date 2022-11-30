1b7b6788dabd5d4d2d6afcb17ca8339a6b87fce0
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-01 00:28:12 +0200

    _

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,16 +7,39 @@ const Wrapper = styled.div`
     margin: auto;
     display: flex;
     align-items: center;
-    padding: 10px;
+    padding: 15px;
     border-radius: var(--border-rad);
     box-shadow: var(--shadow);
     background-color: var(--search-bg)
+`;
+
+const Input = styled.input.attrs({
+    placeholder: 'Search GitHub username...'
+})`
+    border: none;
+    outline: none;
+    max-width: 450px;
+    width: 100%;
+    font-size: 20px;
+    color: var(--gray-200);
+    margin-left: 25px;
+    color: var(--search-color);
+    background: initial;
+`;
+
+const Button = styled.button`
+    border: none; 
+    background-color: var(--main-color);
+    border-radius: var(--border-rad);
+    padding: 15px 25px;
 `;
 
 export const Search = () => {
     return (
         <Wrapper>
             <IconSearch />
+            <Input />
+            <Button>Search</Button>
         </Wrapper>
     );
 };
